e9bbf367717a2d4b745fe08e1a4639858c8ac73a
Arhan Choudhury <arhanchoudhury@gmail.com>
2021-02-04 01:56:14 +0530

    Adding features

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./component/layout/Navbar";
 //import UserItem from "./component/users/UserItem";
 import Users from "./component/users/Users";
 import Search from "./component/users/Search";
+import Alert from "./component/layout/Alert";
 import axios from "axios";
 import "./App.css";
 
@@ -10,9 +11,11 @@ class App extends Component {
   state = {
     users: [],
     loading: false,
+    alert: null,
   };
-  //Life Cycle Method
-  //Whenever the app loads this get fires
+
+  // Life Cycle Method
+  // Whenever the app loads this get fires
   async componentDidMount() {
     //No need to use promises
     this.setState({ loading: true });
@@ -21,14 +24,41 @@ class App extends Component {
     );
     this.setState({ users: res.data, loading: false });
   }
+  //Search Github Users
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  clearUsers = () => {
+    this.setState({ users: [], loading: false });
+  };
+
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+
+    setTimeout(() => {
+      this.setState({ alert: null });
+    }, 5000);
+  };
   //Life Cycle Method
   render() {
+    const { users, loading } = this.state;
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Search />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Alert alert={this.state.alert} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+          />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
--- src/component/users/Search.js
+++ src/component/users/Search.js
@@ -1,16 +1,36 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export class Search extends Component {
   state = {
     text: "",
   };
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired,
+  };
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    if (this.state.text === "") {
+      this.props.setAlert("Please enter something", "light");
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: "" });
+    }
+  };
+
   onChange = (event) => {
-    this.setState({ text: event.target.value });
+    this.setState({ [event.target.name]: event.target.value });
   };
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
-        <form className='form'>
+        <form onSubmit={this.onSubmit} className='form'>
           <input
             type='text'
             name='text'
@@ -24,6 +44,11 @@ export class Search extends Component {
             className='btn btn-dark btn-block'
           />
         </form>
+        {showClear && (
+          <button className='btn btn-light btn-block' onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
