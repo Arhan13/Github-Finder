a68a9198b6628773163a304a275ec3d3a5934f17
Arhan Choudhury <arhanchoudhury@gmail.com>
2021-02-04 03:02:57 +0530

    Adding features

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./component/layout/Navbar";
 //import UserItem from "./component/users/UserItem";
 import Users from "./component/users/Users";
 import Search from "./component/users/Search";
 import Alert from "./component/layout/Alert";
+import About from "./component/pages/About";
+import User from "./component/users/User";
 import axios from "axios";
 import "./App.css";
 
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null,
   };
@@ -33,6 +37,16 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  //Get a single github user
+
+  getUser = async (username) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ user: res.data, loading: false });
+  };
+
   clearUsers = () => {
     this.setState({ users: [], loading: false });
   };
@@ -46,21 +60,46 @@ class App extends Component {
   };
   //Life Cycle Method
   render() {
-    const { users, loading } = this.state;
+    const { user, users, loading } = this.state;
     return (
-      <div className='App'>
-        <Navbar />
-        <div className='container'>
-          <Alert alert={this.state.alert} />
-          <Search
-            searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            showClear={users.length > 0 ? true : false}
-            setAlert={this.setAlert}
-          />
-          <Users loading={loading} users={users} />
+      <Router>
+        <div className='App'>
+          <Navbar />
+          <div className='container'>
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path='/'
+                render={(props) => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path='/about' component={About} />
+              <Route
+                exact
+                path='/user/:login'
+                render={(props) => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    user={user}
+                    loading={loading}
+                  />
+                )}
+              />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
--- src/component/layout/Navbar.js
+++ src/component/layout/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import PropTypes from "prop-types";
-
+import { Link } from "react-router-dom";
 const Navbar = ({ icon, title }) => {
   return (
     <div className='navbar bg-primary'>
@@ -9,6 +9,14 @@ const Navbar = ({ icon, title }) => {
         <i className={icon} style={{ marginRight: "10px" }} />
         {title}
       </h1>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
     </div>
   );
 };
